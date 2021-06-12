1f2afebcf233fa6eecd4d3a32fdb99a5f8299921
jeremyindie <jers@tutamail.com>
2021-06-12 16:27:41 -0400

    a

--- Bubbles_GamJAm/Assets/Scripts/Bubble.cs
+++ Bubbles_GamJAm/Assets/Scripts/Bubble.cs
@@ -14,7 +14,7 @@ public class Bubble : MonoBehaviour
         Physics.gravity = new Vector3(0,floating,0);
         var movement = -Input.GetAxis("Horizontal");
         transform.position += new Vector3(movement,0,0) * Time.deltaTime * MovementSpeed;
-        transform.localScale += new Vector3(-deflateRate,-deflateRate,-deflateRate * Time.deltaTime);
+        transform.localScale += new Vector3(-deflateRate * Time.deltaTime, -deflateRate * Time.deltaTime, -deflateRate * Time.deltaTime);
         
     }
 
