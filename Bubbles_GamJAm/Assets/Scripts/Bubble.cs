b226654f093757637add3d81a919a867b9d39860
DESKTOP-HASFGEJ\chris <chris.bradshaw@live.ca>
2021-06-12 13:03:27 -0400

    added scripts and prefabs

---
+++ Bubbles_GamJAm/Assets/Scripts/Bubble.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bubble : MonoBehaviour
+{
+
+    public float floating;
+    public float MovementSpeed;
+    public float deflateRate;
+
+    void Update()
+    {
+        Physics.gravity = new Vector3(0,floating,0);
+        var movement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movement,0,0) * Time.deltaTime * MovementSpeed;
+        transform.localScale += new Vector3(-deflateRate,-deflateRate,-deflateRate * Time.deltaTime);
+        
+    }
+
+
+}
---
+++ Bubbles_GamJAm/Assets/Scripts/BubblePowerUp.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubblePowerUp : MonoBehaviour
+{
+    public GameObject Bubble;
+    public float increaseInBubbleSize;
+    public Bubble bubbleScript;
+
+    public void OnCollisionEnter(Collision collision)
+    {
+       if (collision.gameObject.tag == "Player")
+        {
+            bubbleScript.floating += 0.5f;
+            Physics.gravity = new Vector3(0,5f,0);
+            Bubble.transform.localScale += new Vector3(increaseInBubbleSize,increaseInBubbleSize,increaseInBubbleSize);
+            Destroy(this.gameObject);
+        }
+          
+       
+    }
+
+    private void DeathAfterTime()
+    {
+        Destroy(this.gameObject,20f);
+    }
+
+}
