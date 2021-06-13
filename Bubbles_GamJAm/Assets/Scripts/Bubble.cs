b646bfc699d64a85f7541bf5c3f611fcc97fb689
jeremyindie <jers@tutamail.com>
2021-06-12 21:32:03 -0400

    changed stuff back, sorry

--- Bubbles_GamJAm/Assets/Scripts/Bubble.cs
+++ Bubbles_GamJAm/Assets/Scripts/Bubble.cs
@@ -11,11 +11,11 @@ public class Bubble : MonoBehaviour
 
     void Update()
     {
-        Physics.gravity = new Vector3(0,floating,0);
+        Physics.gravity = new Vector3(0, floating, 0);
         var movement = -Input.GetAxis("Horizontal");
-        transform.position += new Vector3(movement,0,0) * Time.deltaTime * MovementSpeed;
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
         transform.localScale += new Vector3(-deflateRate * Time.deltaTime, -deflateRate * Time.deltaTime, -deflateRate * Time.deltaTime);
-        
+
     }
 
 
--- Bubbles_GamJAm/Assets/Scripts/BubblePowerUp.cs
+++ Bubbles_GamJAm/Assets/Scripts/BubblePowerUp.cs
@@ -8,28 +8,22 @@ public class BubblePowerUp : MonoBehaviour
     public float increaseInBubbleSize;
     public Bubble bubbleScript;
 
-    private void Awake()
-    {
-        Physics.gravity = new Vector3(0, 5f, 0);
-
-    }
-
     public void OnCollisionEnter(Collision collision)
     {
-       if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player")
         {
             bubbleScript.floating += 0.5f;
-           // Physics.gravity = new Vector3(0,5f,0);
-            Bubble.transform.localScale += new Vector3(increaseInBubbleSize,increaseInBubbleSize,increaseInBubbleSize);
+            Physics.gravity = new Vector3(0, 5f, 0);
+            Bubble.transform.localScale += new Vector3(increaseInBubbleSize, increaseInBubbleSize, increaseInBubbleSize);
             Destroy(this.gameObject);
         }
-          
-       
+
+
     }
 
     private void DeathAfterTime()
     {
-        Destroy(this.gameObject,20f);
+        Destroy(this.gameObject, 20f);
     }
 
 }
