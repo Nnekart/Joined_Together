035a371ab41a1257a0e999113125f97fcc43dede
jeremyindie <jers@tutamail.com>
2021-06-12 23:13:23 -0400

    no message

--- Bubbles_GamJAm/Assets/Rotating.cs
+++ Bubbles_GamJAm/Assets/Rotating.cs
@@ -7,17 +7,20 @@ public class Rotating : MonoBehaviour
     // Start is called before the first frame update
     private float direction = 1;
     private float LerpValue = 0.0f;
-    public float angle = 20f; 
+    public float angle = 20f;
+    public Vector3 startRotation;
+    public float lerpOffset = 0;
     void Start()
     {
-        
+        startRotation = transform.rotation.eulerAngles;
+        LerpValue = lerpOffset;
     }
 
     // Update is called once per frame
     void Update()
     {
         float rotationZ = EasingFunction.Spring(0.0f, angle * direction, LerpValue);
-        transform.rotation = Quaternion.Euler(new Vector3(0.0f, 0.0f, rotationZ));
+        transform.rotation = Quaternion.Euler(new Vector3(0.0f, 0.0f, rotationZ + startRotation.z));
         LerpValue += Time.deltaTime; 
         if (LerpValue > 1.0f)
         {
